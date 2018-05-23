13d9b001b50b67b4ea4c2380c4b58fa169881e41
devson <sonyc5720@gmail.com>
2018-05-23 17:59:04 +0900

    Boot Starter Aspect PostgreSQL Web Lombok

---
+++ src/main/java/com/template/boot/BootMvcTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.template.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcTemplateApplication.class, args);
+	}
+}
